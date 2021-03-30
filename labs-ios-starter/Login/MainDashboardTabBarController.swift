b69a84cac4c3696b3a7fdc3c4a7eeec610ab12eb
Sal562 <salmanamer@gmail.com>
2021-03-30 20:48:49 +0700

    created MainDashboardTabBarController to allow segue from loginViewController

---
+++ labs-ios-starter/Login/MainDashboardTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainDashboardTabBarController.swift
+//  labs-ios-starter
+//
+//  Created by Sal562 on 3/30/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class MainDashboardTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
