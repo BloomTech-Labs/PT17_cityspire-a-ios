0bfec7be4355d6c0f497fb08cb9246ec0f5d5169
Sal562 <salmanamer@gmail.com>
2021-04-01 15:14:26 +0700

    fixed spelling mistake - livabilityLabel

--- labs-ios-starter/Search/NewSearchViewController.swift
+++ labs-ios-starter/Search/NewSearchViewController.swift
@@ -2,26 +2,31 @@
 //  NewSearchViewController.swift
 //  labs-ios-starter
 //
-//  Created by Sal562 on 4/1/21.
+//  Created by Sal B Amer on 4/1/21.
 //  Copyright © 2021 Spencer Curtis. All rights reserved.
 //
 
 import UIKit
 
-class NewSearchViewController: UITabBarController {
-    
+class NewSearchViewController: UIViewController {
     
     //MARK: - Variables
     let networkClient = NetworkClient()
     let city : String? = ""
     let state : String? = ""
     
+//    var fetchedCityData = [CityData]()
+    
     //MARK: - Outlets
     
     @IBOutlet weak var airQualityLabel: UILabel!
     @IBOutlet weak var crimeLabel: UILabel!
     @IBOutlet weak var populationLabel: UILabel!
-    @IBOutlet weak var liveabilityLabel: UILabel!
+    @IBOutlet weak var livabilityLabel: UILabel!
+    
+    @IBOutlet weak var diversityIndex: UILabel!
+    @IBOutlet weak var diversityIndex: UILabel!
+    @IBOutlet weak var diversityIndex: UILabel!
     
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -31,23 +36,28 @@ class NewSearchViewController: UITabBarController {
 //        searchBar.delegate = self
         view.backgroundColor = .white
         
-        fetchCityData(city: "New York", state: "NY")
-        // Do any additional setup after loading the view.
+
+        fetchCityData(city: "Los Angeles", state: "CA")
     }
-    
     
     //get city data
     func fetchCityData(city: String, state: String) -> Void {
-        networkClient.getCityData(city: city, state: state) { (cityData, error) in
+        networkClient.getCityData(city: city, state: state) { [weak self] (cityData, error) in
             if error != nil {
                 print(error?.localizedDescription)
                 return
             }
-            print(cityData)
-//            self.airQualityLabel.text = "The AirQuality Index is: \(String(describing: cityData?.airQualityIndex))"
-//            self.crimeLabel.text = "The CrimeData Index is: \(cityData?.crime)"
-//            self.populationLabel.text = "The Population is: \(cityData!.population)"
-//            self.liveabilityLabel.text = "The Liveability Index is: \(cityData!.livability)"
+            print("Just CiTYDATA \(cityData)")
+            
+            guard let cityData = cityData else { return }
+            
+            DispatchQueue.main.async {
+                self!.airQualityLabel.text = "The AirQuality Index is: \(cityData.airQualityIndex ?? "NA")"
+                self!.crimeLabel.text = "The CrimeData Index is: \(cityData.crime ?? "NA")"
+                self!.populationLabel.text = "The Population is: \(cityData.population ?? 0)"
+                self!.livabilityLabel.text = "The Liveability Index is: \(cityData.livability ?? 0)"
+            }
+
         }
     }
     
