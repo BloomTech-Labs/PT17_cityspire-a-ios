fcbc24655372a572b47979e6097ce9a85b8337cf
Sal562 <salmanamer@gmail.com>
2021-04-11 14:48:34 +0700

    added Extensions.swift for view creation helpers

---
+++ labs-ios-starter/Helpers/Extensions.swift
@@ -0,0 +1,49 @@
+//
+//  Extensions.swift
+//  labs-ios-starter
+//
+//  Created by Sal562 on 4/11/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+// UI Label
+extension UILabel {
+    //initialize
+    convenience init(text: String, font: UIFont, numberOfLines: Int = 1) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+        self.numberOfLines = numberOfLines
+    }
+}
+
+// ImageView
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image : nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+
+// Button Extension
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+    }
+}
+
+
+//custom initializer for customized stackView spacing
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], customSpacing: CGFloat = 0) {
+        self.init(arrangedSubviews : arrangedSubviews)
+        self.spacing = customSpacing
+    }
+}
+
