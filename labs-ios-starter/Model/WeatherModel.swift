b1a21b8edcac9a8fc44b9db901b73e82ea9dd74e
Sal562 <salmanamer@gmail.com>
2021-04-19 22:35:13 +0700

    added weatherIcons

---
+++ labs-ios-starter/Model/WeatherModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherModel.swift
+//  labs-ios-starter
+//
+//  Created by Sal562 on 4/19/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+struct Weather {
+    
+}
